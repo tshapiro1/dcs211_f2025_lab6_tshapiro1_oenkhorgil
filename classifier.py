7296dac14924935d39608226e032bb22a00c9fe4
liettexx <enkhoyuka469@gmail.com>
2025-11-11 10:24:39 -0500

    Added more training data

--- classifier.py
+++ classifier.py
@@ -16,6 +16,32 @@ train = [
     ('Outstanding meal, every dish was fantastic.', 'pos'),
     ('Excellent food and friendly staff.', 'pos'),
     ('This is my new favorite restaurant!', 'pos'),
+    ('The burgers were juicy, flavorful, and satisfying in that deeply American way that only a Five Guys meal can be.', 'pos'),
+    ('The fries with the iconic peel were fantastic and the service was also pretty good.', 'pos'),
+    ('There is a soda fountain with so many drink options. And their fries are just the best you can get.', 'pos'),
+    ('I’m so glad I gave this place a try. The service was great and the food was delicious.', 'pos'),
+    ('Food tasted great and the dessert was unexpectedly yummy.', 'pos'),
+
+    [
+    
+    ('I only tried this place because I was given a gift card.', 'neutral'),
+
+    ("The steaks and burger came out med-rare instead of medium, my kids dont like to see too much blood", "negative"),
+    ("I had to wait for a long time to get my food, the cashier was not even looking at me, he was just talking to his friend.", "negative"),
+    ("The servers are welcoming and always made me feel invited.", "positive"),
+    ("Overall, the sushi was decent.", "neutral"),
+    ("Great sushi, clean place, very quiet set up inside", "positive"),
+    ("The fish was fresh and the sushi was delicious. The only downside was the price.", "neutral"),
+    ("The owners make you feel like family and make sure you have the best service and food.", "positive"),
+    ("The sashimi was fresh and very refreshing.", "positive"),
+    ("The sushi was good, but the service was slow.", "neutral"),
+    ("It was in such a nice venue, the environment was clean and had such a pleasing atmosphere to it.", "positive"),
+    ("The burrito was not good, the sauce was too sweet and the rice was not cooked well.", "negative"),
+    ("It's a pretty straightforward ordering process as well where you queue and order, and basically just wait a while.", "neutral"),
+    ("One of the best tacos I've had in a while.", "positive"),
+    ("The whole vibe of the store is worthy, the service was amazing so friendly, professional, and quick. ", "positive"),
+    ("There was hair in the food, the salsa was not good, and the chips were not fresh.", "negative")
+]
     
     # Negative reviews
     ('This restaurant is terrible.', 'neg'),
@@ -26,6 +52,9 @@ train = [
     ('The chicken was undercooked and unsafe to eat.', 'neg'),
     ('Awful experience, the place was dirty.', 'neg'),
     ('Would not recommend, complete waste of money.', 'neg'),
+    ('I felt ripped off. The French fries were too salty and cooked in burnt oil.', 'neg'),
+    ('The cashier was rude and careless. He didn’t even bother to speak, everything was on signs.', 'neg'),
+    ('He took out the trash and hit every customer on his way, he even drop a table on a customer. Disappointed of the service.', 'neg'),
     
     # Neutral reviews
     ('The food was okay, nothing special.', 'neu'),
